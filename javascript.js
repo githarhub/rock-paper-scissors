36b2292c10fc143d8d4b31def1a7934fd0f57676
githarhub <179049148+githarhub@users.noreply.github.com>
2024-10-17 19:35:33 +0630

    feat: return rock, paper or scissor
    created return rock, paper or scissor in getComputerChoice() in javascript.js
    if rpsNumber is less than or equal to  0.3333333333333333 then return rock
    else if rpsNumber is greater than 0.3...3 (16 times) and less than or equal to 0.6...6 (16 times) then return paper
    else return scissor

--- javascript.js
+++ javascript.js
@@ -1,3 +1,14 @@
 function getComputerChoice() {
-    let  rpsNumber= Math.random();
+    let rpsNumber = Math.random();
+
+    if (rpsNumber <= 0.3333333333333333) {
+        return "rock";
+    }
+    else if (rpsNumber > 0.3333333333333333 && rpsNumber <= 0.6666666666666666) {
+        return "paper";
+    }
+    else {
+        return "scissor";
+    }
+
 }
