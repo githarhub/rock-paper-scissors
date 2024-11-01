ef6ade3acaa4e931d2d4bb0ae3ec5a67b323b72a
githarhub <179049148+githarhub@users.noreply.github.com>
2024-11-01 21:49:15 +0630

    feat: play when user click button
    removed getHumanChoice() in javascript.js
    added pickAndPlay() that add to event listener that play game when click

--- javascript.js
+++ javascript.js
@@ -1,8 +1,13 @@
+// get reference of buttons
+const rockButton = document.querySelector(".rock");
+const paperButton = document.querySelector(".paper");
+const scissorsButton = document.querySelector(".scissors");
+
 // create function that return one of rock paper and scissors
 function getComputerChoice() {
 
     // variable that return between 0 and 1
-    let rpsNumber = Math.random();
+    const rpsNumber = Math.random();
 
     if (rpsNumber <= 0.3333333333333333) {
         return "rock";
@@ -18,28 +23,22 @@ function getComputerChoice() {
 
 }
 
-// create function that get one of rock paper scissors input from user
-function getHumanChoice() {
 
-    // loop until user input valid input
-    while (true) {
-        // prompt to user to type valid input
-        let userInput = prompt("Choose one of rock, paper or scissors").toLowerCase();
 
-        // check if user type valid input and if true return and if not alert and ask again
-        if (userInput == "rock" || userInput == "paper" || userInput == "scissors") {
-            console.log(userInput);
-            return userInput;
-        }
+// play when user click button
+function pickAndPlay(pickButton) {
+    pickButton.addEventListener("click", () => {
+        const humanChoice = pickButton.textContent.toLowerCase();
+        const computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    });
 
-        else {
-            alert("Type only one of rock, paper or scissors");
-        }
-    }
-}
+};
+
 // global variable that count winner score of human and computer
 let humanScore = 0;
 let computerScore = 0;
+
 
 // create function that check winner and increment winner Score and log winner
 function playRound(humanChoice, computerChoice) {
@@ -115,4 +114,6 @@ function playGame () {
 }
 
 // call the function
-playGame();
+pickAndPlay(rockButton);
+pickAndPlay(paperButton);
+pickAndPlay(scissorsButton);
